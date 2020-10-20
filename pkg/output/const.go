712eec4c9f509fd6fd97098cdbb1788dfc50b552
Jinone <44525519+Jinone@users.noreply.github.com>
2020-10-20 18:13:58 +0800

    Update const.go

--- pkg/output/const.go
+++ pkg/output/const.go
@@ -3,7 +3,7 @@
 package output
 
 const (
-	TERMINAL_CLEAR_LINE = "\r\x1b[2K"
+	TERMINAL_CLEAR_LINE = ""
 	ANSI_CLEAR          = "\x1b[0m"
 	ANSI_RED            = "\x1b[31m"
 	ANSI_GREEN          = "\x1b[32m"
